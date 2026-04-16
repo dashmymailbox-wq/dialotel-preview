63ecf49822678a1e72eb1059e2f2bb23e57b7b37
Olivier <olivier@local>
2026-04-16 16:10:10 +0200

    update: enqueue.php

--- clients/dialotel/wordpress/includes/enqueue.php
+++ clients/dialotel/wordpress/includes/enqueue.php
@@ -66,6 +66,7 @@ body { background-color: #ffffff !important; margin: 0 !important; }
     contain: none !important;
     will-change: auto !important;
     perspective: none !important;
+    background: transparent !important;
 }
 
 ' );
